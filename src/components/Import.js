598c8884f13376e51d80d5dc9e9f4e98a91fc7a3
Stephen <stephen.lyssy@gmail.com>
2022-12-04 20:17:01 -0600

    Added Import component and wired up redux to use thunk to fetch the data.

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,11 +1,50 @@
-import React from 'react'
+import React from 'react';
+
+import {
+  Container,
+  Button,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+} from '@mui/material';
+
+import DeleteIcon from '@mui/icons-material/Delete';
 
 const Import = (props) => {
-    // fill out this component
+  // fill out this component
+  return (
+    <Container maxWidth='md'>
+      <Button onClick={props.fetchMakes} variant='contained'>
+        Import
+      </Button>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>ID</TableCell>
+            <TableCell>Make</TableCell>
+            <TableCell>Actions</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {props.makes.map((car, idx) => (
+            <TableRow key={car.MakeId}>
+              <TableCell>{car.MakeId}</TableCell>
+              <TableCell>{car.MakeName}</TableCell>
+              <TableCell>
+                <DeleteIcon
+                  // add onClick method here
+                  onClick={() => props.removeCar(idx)}
+                  className='icon text-red'
+                />
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Container>
+  );
+};
 
-    return (
-        <p>Import Component</p>
-    )
-}
-
-export default Import
+export default Import;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,11 @@
-import { createStore } from 'redux'
-import reducers from './reducers'
-import state from './state'
+//? Step 3 README: import applyMiddleware next to createStore
+import { createStore, applyMiddleware } from 'redux';
 
-export default createStore(reducers, state)
+//? Step 3 README: import thunk from 'redux-thunk'
+import thunk from 'redux-thunk';
+
+import reducers from './reducers';
+import state from './state';
+
+//? Step 3 README: Add applyMiddleware(thunk) as last argument in createStore method.
+export default createStore(reducers, state, applyMiddleware(thunk));
